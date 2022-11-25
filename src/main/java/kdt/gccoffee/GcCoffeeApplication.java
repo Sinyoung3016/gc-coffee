df133af869319acc3d519351e225e4c057dcbd98
Fortune00 <sinyoungbok99@gmail.com>
2022-11-25 16:08:27 +0900

    chore: 스프링 프로젝트 생성

---
+++ src/main/java/kdt/gccoffee/GcCoffeeApplication.java
@@ -0,0 +1,13 @@
+package kdt.gccoffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcCoffeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcCoffeeApplication.class, args);
+	}
+
+}
