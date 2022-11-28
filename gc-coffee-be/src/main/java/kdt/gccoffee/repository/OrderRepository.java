ace9c7184528db25133f21fdd89355a799e89825
Fortune00 <sinyoungbok99@gmail.com>
2022-11-28 23:42:32 +0900

    feat: Order(주문) 저장소 생성

---
+++ gc-coffee-be/src/main/java/kdt/gccoffee/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package kdt.gccoffee.repository;
+
+import kdt.gccoffee.model.Order;
+
+public interface OrderRepository {
+
+    Order insert(Order order);
+
+}
--- gc-coffee-be/src/main/resources/schema.sql
+++ gc-coffee-be/src/main/resources/schema.sql
@@ -1,10 +1,36 @@
 CREATE TABLE if not exists products
 (
-    product_id   BINARY(16) PRIMARY KEY,
+    product_id   BINARY(16)   PRIMARY KEY,
     product_name VARCHAR(20)  NOT NULL,
     category     VARCHAR(50)  NOT NULL,
     price        BIGINT       NOT NULL,
-    descriptions VARCHAR(500),
-    created_at   DATETIME(6) NOT NULL,
-    updated_at   DATETIME(6) NOT NULL
+    descriptions VARCHAR(500) DEFAULT NULL,
+    created_at   DATETIME(6)  NOT NULL,
+    updated_at   DATETIME(6)  DEFAULT NULL
+);
+
+CREATE TABLE if not exists orders
+(
+    order_id     BINARY(16)    PRIMARY KEY,
+    email        VARCHAR(50)   NOT NULL,
+    address      VARCHAR(200)  NOT NULL,
+    postcode     VARCHAR(200)  NOT NULL,
+    order_status VARCHAR(50)   NOT NULL,
+    created_at   DATETIME(6)   NOT NULL,
+    updated_at   DATETIME(6)   DEFAULT NULL
+);
+
+CREATE TABLE if not exists order_items
+(
+    seq          BIGINT        PRIMARY KEY AUTO_INCREMENT,
+    order_id     BINARY(16)    NOT NULL,
+    product_id   BINARY(16)    NOT NULL,
+    category     VARCHAR(50)   NOT NULL,
+    price        BIGINT        NOT NULL,
+    quantity     INT           NOT NULL,
+    created_at   DATETIME(6)   NOT NULL,
+    updated_at   DATETIME(6)   DEFAULT NULL,
+    INDEX (order_id),
+    CONSTRAINT fk_order_items_to_order FOREIGN KEY (order_id) REFERENCES orders (order_id) ON DELETE CASCADE,
+    CONSTRAINT fk_order_items_to_product FOREIGN KEY (product_id) REFERENCES products (product_id)
 );
